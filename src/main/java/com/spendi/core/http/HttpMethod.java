35b2f7249a98993e73f880e4bc4a0ef76430bf0f
chyVacheck <dmytro.shah.work@gmail.com>
2025-08-28 19:27:08 +0200

    feat(HttpMethod): создать enum http методов

---
+++ src/main/java/com/spendi/core/http/HttpMethod.java
@@ -0,0 +1,13 @@
+
+/**
+ * @file HttpMethod.java
+ * @module core/http
+ *
+ * @author Dmytro Shakh
+ */
+
+package com.spendi.core.http;
+
+public enum HttpMethod {
+	GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD
+}
