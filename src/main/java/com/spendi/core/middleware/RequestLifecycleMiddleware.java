a9bcee4408cdca8cbd5576aea6315534645b0d83
chyVacheck <dmytro.shah.work@gmail.com>
2025-08-28 22:01:27 +0200

    feat: создать RequestLifecycleMiddleware

---
+++ src/main/java/com/spendi/core/middleware/RequestLifecycleMiddleware.java
@@ -0,0 +1,67 @@
+
+/**
+ * @file RequestLifecycleMiddleware.java
+ * @module core/middleware
+ * 
+ * @see BaseMiddleware
+ * 
+ * @description
+ * Финальная middleware, завершающая жизненный цикл запроса:
+ * - рассчитывает время обработки (нс + мс),
+ * - выставляет заголовки (X-Request-Id, X-Response-Time, X-Response-Time-Nanos, X-Request-Heavy),
+ * - логирует итог запроса.
+ *
+ *
+ * @author Dmytro Shakh
+ */
+
+package com.spendi.core.middleware;
+
+/**
+ * ! my imports
+ */
+import com.spendi.core.base.BaseMiddleware;
+import com.spendi.core.base.http.HttpContext;
+import com.spendi.core.base.http.MiddlewareChain;
+
+public class RequestLifecycleMiddleware extends BaseMiddleware {
+	// порог «тяжёлого» запроса в мс
+	private static final long HEAVY_REQUEST_THRESHOLD_MS = 800;
+
+	public RequestLifecycleMiddleware() {
+		super(RequestLifecycleMiddleware.class.getSimpleName());
+
+	}
+
+	@Override
+	public void handle(HttpContext ctx, MiddlewareChain chain) throws Exception {
+		try {
+			chain.next();
+		} finally {
+			// фрагмент внутри finally
+			long durationNanos = System.nanoTime() - ctx.getStartNanos();
+			long durationMs = durationNanos / 1_000_000;
+
+			ctx.res().header("X-Request-Id", ctx.getRequestId());
+			ctx.res().header("X-Response-Time-Nanos", String.valueOf(durationNanos));
+			ctx.res().header("X-Response-Time", durationMs + "ms");
+
+			boolean isHeavyRequest = durationMs > HEAVY_REQUEST_THRESHOLD_MS;
+			if (isHeavyRequest) {
+				ctx.res().header("X-Request-Heavy", "true");
+				warn("Heavy request", detailsOf(
+						"requestId", ctx.getRequestId(),
+						"durationMs", durationMs,
+						"durationNanos", durationNanos,
+						"path", ctx.req().path()));
+			} else {
+				debug("Request processed", detailsOf(
+						"requestId", ctx.getRequestId(),
+						"durationMs", durationMs,
+						"durationNanos", durationNanos,
+						"path", ctx.req().path()));
+			}
+
+		}
+	}
+}
